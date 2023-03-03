13cf9dd4c7f7ecdebdd1a20a5c95b25bf6d30139
Mo <myatmo@outlook.com>
2023-03-03 12:22:19 -0600

    changes on neptune

--- nnunet/utilities/neptune_config.py
+++ nnunet/utilities/neptune_config.py
@@ -1,20 +1,21 @@
+import os
 import neptune.new as neptune
 
-PROJECT_NAME = "UMN-RC-1/Fatchecker"
-API_TOKEN = "eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiIzOGFiYmZkMS1jY2VmLTQ1YWYtYjJjZC0xMjc0MTI4MzNiZTAifQ=="
+NEPTUNE_PROJECT = os.getenv("NEPTUNE_PROJECT")
+#NEPTUNE_API_TOKEN = os.getenv("NEPTUNE_API_TOKEN")
 
 def config_run(id=None):
     if id is None:
         run = neptune.init_run(
-                project=PROJECT_NAME,
-                api_token=API_TOKEN,
+                project=NEPTUNE_PROJECT,
+                #api_token=NEPTUNE_API_TOKEN,
             )
 
     else:
         run = neptune.init_run(
-                project=PROJECT_NAME,
-                api_token=API_TOKEN,
-                with_id = id,
+                project=NEPTUNE_PROJECT,
+                #api_token=NEPTUNE_API_TOKEN,
+                with_id = id
             )
-
+    
     return run
